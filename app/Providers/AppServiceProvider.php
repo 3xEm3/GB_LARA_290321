70264580c8c5fb5abccd5d138169d27be6e3588c
vvo <ex3me@ti-it.net>
2021-04-26 09:27:07 +0300

    task4
    1. Создать форму для получения данных от пользователя. Это может быть форма обратной связи или форма заказа на получение выгрузки данных.
    2. Добавить приемочные тесты на 1 -2 страницы (список новостей, карточки новости).

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $menu = [
+            [
+                'title' => 'Новости',
+                'alias' => 'news::categories'
+            ],
+            [
+                'title' => 'Отзывы',
+                'alias' => 'feedback::create'
+            ],
+            [
+                'title' => 'Админка',
+                'alias' => 'admin::news::index'
+            ],
+        ];
+
+        View::share('menu', $menu);
     }
 }

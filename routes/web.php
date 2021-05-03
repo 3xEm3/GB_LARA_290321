d6a9df0992da36a8e2f42a645b0dc32d9a5203fa
vvo <ex3me@ti-it.net>
2021-05-03 13:37:38 +0300

    merge with task4

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,72 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\NewsController;
+use \App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\Admin\AdminNewsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * Новости
+ */
+Route::group([
+    'prefix' => 'news',
+    'as' => 'news::'
+], function() {
+
+    Route::get('/', [NewsController::class, 'index'])
+        ->name("categories");
+
+    Route::get('/card/{id}', [NewsController::class, 'card'])
+        ->where('id', '[0-9]+')
+        ->name('card');
+
+    Route::get('/{categoryId}', [NewsController::class, 'list'])
+        ->where('id', '[0-9]+')
+        ->name('list');
+
+});
+
+/**  Отзывы */
+
+Route::group([
+    'prefix' => 'feedback',
+    'as' => 'feedback::'
+], function() {
+    Route::get('/create',[FeedbackController::class, 'create'])
+        ->name('create');
+    Route::post( '/save',[FeedbackController::class, 'save'])
+        ->name('save');
+
+});
+
+
+
+
+/** Админка новостей */
+Route::group([
+    'prefix' => '/admin/news',
+    'as' => 'admin::news::',
+], function () {
+    Route::get('/', [AdminNewsController::class, 'index'] )
+        ->name('index');
+    Route::get('/create',[AdminNewsController::class, 'create'])
+        ->name('create');
+    Route::post( '/save',[AdminNewsController::class, 'save'])
+        ->name('save');
+    Route::get('/update',[AdminNewsController::class, 'update'])
+        ->name('update');
+    Route::get('/delete',[AdminNewsController::class, 'delete'])
+        ->name('delete');
+    Route::get('/card/{id}',[AdminNewsController::class, 'card'])
+        ->where('id', '[0-9]+')
+        ->name('card');
+    Route::get('/{categoryId}', [AdminNewsController::class, 'list'])
+        ->where('id', '[0-9]+')
+        ->name('list');
+
 });
